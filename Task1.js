4040ede67842bd42a61d2987c8ea1489133926b3
Maro Sahakyan <mar90.sahakyan@gmail.com>
2021-09-15 01:28:18 +0400

    add Task1 and Task2

---
+++ Task1.js
@@ -0,0 +1,23 @@
+
+console.log("Mar" + true);
+console.log("Mar" + 10);
+console.log(10 + true);
+console.log("Mar" * true);
+console.log("Mar" * 10);
+console.log(10 * true);
+console.log("Mar" / true);
+console.log("Mar" / 10);
+console.log(10 / true);
+
+
+
+
+
+
+
+
+
+/*Выполнить сложение различных типов(string+boolean, string+number, number+boolean)
+Выполнить умножение различных типов(string * boolean, string * number, number * boolean)
+Выполнить деление различных типов(string/boolean, string/number, number/Boolean)
+Выполнить явное преобразование(number, string, boolean)*/
