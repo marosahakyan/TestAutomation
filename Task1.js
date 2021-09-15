43a3d1d955e05550599e7c7e44f6de6c3ab0e49c
Maro Sahakyan <mar90.sahakyan@gmail.com>
2021-09-16 02:52:53 +0400

    Refactoring

--- Task1.js
+++ Task1.js
@@ -1,23 +1,24 @@
-
+//Выполнить сложение различных типов(string+boolean, string+number, number+boolean)
 console.log("Mar" + true);
 console.log("Mar" + 10);
 console.log(10 + true);
+
+//Выполнить умножение различных типов(string * boolean, string * number, number * boolean)
 console.log("Mar" * true);
 console.log("Mar" * 10);
 console.log(10 * true);
+
+//Выполнить деление различных типов(string/boolean, string/number, number/Boolean)
 console.log("Mar" / true);
 console.log("Mar" / 10);
 console.log(10 / true);
 
 
+//Выполнить явное преобразование(number, string, boolean)
+console.log(Number('7') * Number('4'));
 
+let str = "123";
+console.log(typeof str);
 
+console.log(Boolean(''));  
 
-
-
-
-
-/*Выполнить сложение различных типов(string+boolean, string+number, number+boolean)
-Выполнить умножение различных типов(string * boolean, string * number, number * boolean)
-Выполнить деление различных типов(string/boolean, string/number, number/Boolean)
-Выполнить явное преобразование(number, string, boolean)*/
